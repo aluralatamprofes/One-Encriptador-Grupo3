b1fdf4b637781dd831f706fedfe41fcac0411785
genesysaluralatam <genessys.rondon@alura.com.br>
2022-08-18 14:17:35 -0300

    corrección string

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ function btnEncriptar() {
 }
 
 function encriptar(stringEncriptada) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o","ober"], ["u","ufat"]];
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
     for(let i=0; i < matrizCodigo.length; i++) {
         if(stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-        }        
-        return stringEncriptada;
+        }                
     }
+    return stringEncriptada;
 
     
 }
